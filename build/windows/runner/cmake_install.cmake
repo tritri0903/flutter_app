32bf1910c3be30dd1b09a9396d6e187d69de09d5
tritr0903 <tristan.scropetta@hotmail.com>
2023-07-15 18:17:35 +0200

    Init

--- build/windows/runner/cmake_install.cmake
+++ build/windows/runner/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Tristan/OneDrive/Projet/flutter_application_2/windows/runner
+# Install script for directory: C:/Users/Tristan/OneDrive/Projet/FlutterProjet/flutter_app/windows/runner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
